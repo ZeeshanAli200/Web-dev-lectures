f9a7eef4f11818164622a19a87b94e043d58b6d9
ZeeshanAli200 <zeeshan998811@gmail.com>
2021-04-01 12:36:37 +0500

    31march

---
+++ JS/31-3-21 js/app.js
@@ -0,0 +1,26 @@
+// alert("hellow world");
+// console.log("hellow world");
+
+
+// var userName="zeeshan";
+
+// console.log(userName);
+// console.log(userName);
+// console.log(userName);
+
+
+// var inp1=prompt("whats my name??");
+
+// console.log(inp1);
+
+// var num1=5;
+// var num2=10
+// var num3=6
+// var adsub=num1-num2;
+// console.log(adsub);
+
+// var mod=num2%num3;
+// console.log(mod);
+
+// var power=num3**2;
+// console.log(power);
