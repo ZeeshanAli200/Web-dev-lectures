7ccc3f073ff619b13cb2730a1a96369790529cae
Abdul <basit.artistic@gmail.com>
2021-04-02 10:57:49 +0500

    class of if conditions

---
+++ JS/2-4-21 js ifcond/app.js
@@ -0,0 +1,71 @@
+// var userName;
+
+
+// var fullName="Zeeshan Ali";
+
+// var msg="Hello World";
+
+// alert(msg);
+
+// var name="Zeeshan "
+
+// var age=19;
+
+// var uni="KU"
+
+// var course="Mobile and cloud";
+
+
+// alert('PIZZA'+'\n'+'PIZZ'+'\n'+'PIZ'+'\n'+'PI'+'\n'+'P');
+
+
+// var numofch=2;
+// var partner='Barbara'
+
+// var count='hungary'
+// var job='mern developer'
+
+// alert('You will be a '+job+' in '+count+' and married to '+partner+' with '+ numofch+' kids.')
+
+
+// // 
+// document.write("<p>Yah! i can write HTML content through JS.</p>")
+
+
+// var a=13,b=9;
+// console.log('Sum of ' + a + ' and ' + b + ' is ',a + b);
+
+// console.log('Result of subtraction ' + a + ' from ' + b + ' is ',a - b);
+
+// console.log('Result of Multiplication ' + a + ' from ' + b + ' is ',a * b);
+// console.log('Result of division ' + a + ' from ' + b + ' is ',a / b);
+// console.log('Result of mod ' + a + ' from ' + b + ' is ',a % b);
+
+// var inp1=+prompt("Enter Num1:")
+// var inp2=+prompt("Enter Num2:")
+
+// var ans=inp1+inp2;
+
+// console.log("Prompt Answer:",ans)
+
+
+// var tab1=+prompt("NUM1:")
+// var tab2=1;
+
+// document.write(tab1+' x '+ tab2 +' = '+ tab1*tab2++ +'\n');
+
+
+// var monName=prompt("Enter Month in num:");
+
+// if(monName==='Ramzan'){
+//     console.log('welcome to the month of blessings')
+// }
+
+var qual=prompt("Qualification:");
+var elgible='matric'
+if(  qual  ===elgible){
+    console.log('welcome to the Saylani mass training')
+}
+else{
+    console.log('you r not eligible!!')
+}
